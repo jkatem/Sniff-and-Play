a2c9437a78b09cd3b9aed9f910f5fe951c0b759b
jkatem <jenmiao@gmail.com>
2020-07-27 13:23:24 -0700

    add omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
-  # resources :dogs, except: [:create, :update, :delete]
-
-  resources :photos
-
  
   resources :users, only: [:show, :create, :new] do 
     resources :dogs, only: [:new, :show, :index, :edit]
   end 
 
   resources :dogs do
-    resources :photos, only: [:new, :show]
+    resources :photos, only: [:new, :create, :show, :index, :edit]
   end
 
   root 'homepage#home'
@@ -19,12 +14,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get '/signup', to: 'users#new'
   delete '/logout', to: 'sessions#destroy'
-  
-  # get 'users/:id/dogs/:id', to: 'dogs#show'
 
-
-
-
+  get '/auth/google_oauth2/callback' => 'sessions#omniauth'
 # need nested resources if i want the path to go to creating your dog after signing up?? Do I want this only for when creating dog profile? 
 
 end
