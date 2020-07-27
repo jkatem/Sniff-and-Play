e0a1d8d6473b30aab022668d7ed0ef3a57152612
jkatem <jenmiao@gmail.com>
2020-07-27 13:22:15 -0700

    add omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,15 @@ class User < ApplicationRecord
     has_many :dogs
     has_many :comments
     has_many :photos, through: :comments
-    has_many_attached :dog_photos
-
-    validates :email, uniqueness: true
+  
+    validates :email, presence: true
  
     has_secure_password
+
+    def self.from_omniauth(auth)
+        self.find_or_create_by(email: auth[:info][:email]) do |u|
+            u.password = SecureRandom.hex
+        end
+    end
 
 end
