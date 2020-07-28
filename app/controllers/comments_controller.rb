03e35729ba03f4cdef151634ba08335608401655
jkatem <jenmiao@gmail.com>
2020-07-27 23:34:34 -0700

    build comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,19 @@
 class CommentsController < ApplicationController
 
-    def index
-        # show list of all the comments for a photo
-    end
-    
-    def new
-        @comment = Comment.new
-    end
-    
+    before_action :set_comment
+   
     def create
+        @comment = @photo.comments.new(comment_params)
 
     end
 
-    def show
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :dog_id, :photo_id, :user_id)
+    end
+
+    def set_comment
+        @comment = Comment.find(params[:photo_id])
     end
 end
