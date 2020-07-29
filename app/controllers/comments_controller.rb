bcfb71751d64e3b0dba6cf89461d616f6c33cdd8
jkatem <jenmiao@gmail.com>
2020-07-28 21:26:16 -0700

    create my comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,27 @@
 class CommentsController < ApplicationController
 
-    before_action :set_comment
+    before_action :set_comment, only: [:edit, :destroy, :update, :show]
    
     def create
-        @comment = @photo.comments.new(comment_params)
+        
+ # find photo by params photo_id
+        @photo = Photo.find_by_id(params[:photo_id])
+        # create/build comment for photo 
+        # also set user info
+        comment = Comment.new(comment_params)
+        comment.photo_id = @photo.id 
+        comment.dog_id = @photo.dog_id
+        comment.user_id = Dog.find(@photo.dog_id).user_id
+        comment.save
 
+        redirect_back(fallback_location: root_path)
     end
+
 
     private
 
     def comment_params
-        params.require(:comment).permit(:content, :dog_id, :photo_id, :user_id)
+        params.require(:comment).permit(:content, :dog_id)
     end
 
     def set_comment
